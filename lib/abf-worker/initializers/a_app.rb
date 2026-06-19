10a89e171384d6c48f2b23bdb6e219c19c146170
alex stefanov <alexander@mezon.ru>
2026-06-19 12:34:56 +0300

    adjust publisher to modern ruby

--- lib/abf-worker/initializers/a_app.rb
+++ lib/abf-worker/initializers/a_app.rb
@@ -8,5 +8,5 @@ Thread.abort_on_exception = true
 ROOT = File.dirname(__FILE__) + '/../../../'
 
 APP_CONFIG = YAML.load(ERB.new(File.read(File.join(ROOT, "config", "application.yml"))).result)
-Dir.mkdir(APP_CONFIG['output_folder']) if !Dir.exists?(APP_CONFIG['output_folder'])
-Dir.mkdir(ROOT + '/container') if !Dir.exists?(ROOT + '/container')
+Dir.mkdir(APP_CONFIG['output_folder']) if !Dir.exist?(APP_CONFIG['output_folder'])
+Dir.mkdir(ROOT + '/container') if !Dir.exist?(ROOT + '/container')
